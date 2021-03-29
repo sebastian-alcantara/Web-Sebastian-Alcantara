087fcb63880f4a0afa0e9666a8072d10a2c95ccd
Sebastian Alcantara <sebastian.h.alcantara@gmail.com>
2021-03-29 15:05:39 -0300

    Aria-label

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -143,7 +143,7 @@ export default function Home({ data }) {
             <ContactLinks />
           </Bounce>
           <Bounce up>
-            <button className="section-scroll-button" onClick={handleNextClick} name="section-scroll-button">
+            <button className="section-scroll-button" onClick={handleNextClick} aria-label="section-scroll-button">
             <svg className="svg-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 491.996 491.996">
                 <path d="M484.132,124.986l-16.116-16.228c-5.072-5.068-11.82-7.86-19.032-7.86c-7.208,0-13.964,2.792-19.036,7.86l-183.84,183.848
                 L62.056,108.554c-5.064-5.068-11.82-7.856-19.028-7.856s-13.968,2.788-19.036,7.856l-16.12,16.128
